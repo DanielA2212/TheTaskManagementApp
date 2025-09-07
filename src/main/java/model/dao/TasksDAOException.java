ffc81d1256c1bf8ef8ab78b5fca4415d0ab5fa7c
DanielA2212 <danielawork98@gmail.com>
2025-09-07 04:18:09 +0300

    Added @ To Code

--- src/main/java/model/dao/TasksDAOException.java
+++ src/main/java/model/dao/TasksDAOException.java
@@ -1,9 +1,12 @@
 package model.dao;
 
+import java.io.Serial;
+
 /**
  * Exception class for DAO operations
  */
 public class TasksDAOException extends Exception {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public TasksDAOException(String message) {
