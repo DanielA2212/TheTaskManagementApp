6a10b6c645b9a06ce0a61cdc6a9a8d7f837899f8
DanielA2212 <danielawork98@gmail.com>
2025-09-11 20:59:16 +0300

    Ummmm..... I Dont Know

---
+++ Stuff For Help/project of david/src/il/ac/hit/project/main/viewmodel/strategy/SortByStateStrategy.java
@@ -0,0 +1,38 @@
+package il.ac.hit.project.main.viewmodel.strategy;
+
+import il.ac.hit.project.main.model.task.ITask;
+import il.ac.hit.project.main.model.task.TaskState;
+import il.ac.hit.project.main.model.task.ToDoState;
+import il.ac.hit.project.main.model.task.InProgressState;
+import il.ac.hit.project.main.model.task.CompletedState;
+
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Sorting strategy that orders tasks by their current workflow state.
+ */
+public class SortByStateStrategy implements ISortingStrategy {
+
+    /**
+     * Sorts tasks by a predefined state order: To Do, InProgress, Completed.
+     *
+     * @param tasks a mutable list of tasks; reordered in place
+     */
+    @Override
+    public void sort(List<ITask> tasks) {
+        tasks.sort(Comparator.comparingInt(task -> {
+            TaskState state = task.getState();
+            if (state instanceof ToDoState) {
+                return 1;
+            }
+            if (state instanceof InProgressState) {
+                return 2;
+            }
+            if (state instanceof CompletedState) {
+                return 3;
+            }
+            return 4;
+        }));
+    }
+}
